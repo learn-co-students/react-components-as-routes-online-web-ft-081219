9178e84826f5a1218d66baedc3c3a53ddc91acfd
Justina Kuchta <justyna.kuchta@gmail.com>
2020-02-01 15:55:52 -0500

    Done.

---
+++ src/Messages.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Messages extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <h3>Lorem proverbs</h3>
+                    <div>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, magni officia facere iste amet necessitatibus dolorem quam voluptatem veniam delectus, aspernatur hic alias mollitia, at perferendis culpa dolores accusantium laboriosam!</p>
+                    </div>
+                    <div>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim voluptatibus illum incidunt sit provident delectus assumenda omnis. Eaque repellat placeat impedit harum earum consequatur ipsam aliquam, fugiat, eos quis praesentium! </p>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Messages;
